1b27b5cdb091a10b93b4cd984437fa0d047203e7
ykharrat <yasmine.kharrat@covectors.io>
2023-11-17 11:35:27 +0100

    added path function

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { assetPrefix: "/blog" };
+const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_URL,
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+};
 
 module.exports = nextConfig;
---
+++ src/utils/getAssetBasePath.ts
@@ -0,0 +1,5 @@
+const prefix = process.env.NEXT_PUBLIC_ASSET_PREFIX;
+
+export default function getAssetBasePath(asset: string) {
+  return `${prefix}/${asset}`.replace("//", "/");
+}
