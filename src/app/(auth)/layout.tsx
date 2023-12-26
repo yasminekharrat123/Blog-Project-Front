472ee10cb5ca4c18967817064ab8d3e044b36c07
Skander Tebourbi <alexandertebourb@gmail.Com>
2023-12-26 21:13:41 +0000

    feat: appearance of register component

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,8 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
+    // TODO: replace h-screen with a more approriate height length when parent layout is ready
+
     return (
-        <div>
-            <h1>Layout</h1>
+        <div className="w-full h-screen flex justify-center items-center">
             {children}
         </div>
     );
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,14 @@
+import { forwardRef } from "react";
 import tw from "tailwind-styled-components";
 
 export type InputProps = React.DetailedHTMLProps<
     React.InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
->;
+> & {
+    label?: string;
+};
 
-export const Input = tw.input<InputProps>`
+const InputInner = tw.input<InputProps>`
     transition-all
     px-4
     py-2
@@ -20,3 +23,18 @@ export const Input = tw.input<InputProps>`
     disabled:ring-primary-500
     disabled:text-neutral-400
 `;
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({ label, ...rest }, ref) => {
+        if (!label) {
+            return <InputInner {...rest} ref={ref} />;
+        }
+
+        return (
+            <div className="flex flex-col gap-1">
+                <label htmlFor={rest.id}>{label}</label>
+                <InputInner {...rest} ref={ref} />
+            </div>
+        );
+    }
+);
